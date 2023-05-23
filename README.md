c5266c04b7b16e286d7b4ed9499b09094efbcc9c
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-23 14:58:13 +0800

    feat(locate-parent): add feature to locate a file in parent dir

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # path-locator
-A rust pacakage for locating a path in the parent and child directories
+
+A rust pacakage for locating a path in the parent directories. This can be used
+to read config files or help locating the project root. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PathError {
+    NotFound,
+}
+
+unsafe impl Send for PathError {}
+unsafe impl Sync for PathError {}
+
+pub fn find(
+    path: impl AsRef<Path>,
+    name: impl AsRef<Path>,
+) -> Result<PathBuf, PathError> {
+    let mut path = path.as_ref();
+    let name = name.as_ref();
+
+    loop {
+        let candidate = path.join(&name);
+        if candidate.exists() {
+            return Ok(candidate);
+        }
+
+        path = match path.parent() {
+            Some(parent) => parent,
+            None => return Err(PathError::NotFound),
+        };
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +33,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_find() {
+        let path = Path::new("src");
+        let name = Path::new(".gitignore");
+        let res = find(path, name);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), Path::new(".gitignore"));
     }
 }
